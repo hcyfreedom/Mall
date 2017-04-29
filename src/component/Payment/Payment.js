b3da831182755d9154b357156f81070492d20462
hcy <956885844@qq.com>
2017-04-29 14:46:55 +0800

    hcy

--- src/component/Payment/Payment.js
+++ src/component/Payment/Payment.js
@@ -9,11 +9,26 @@ import {post} from "../../http/http"
 import * as mallActions from '../../action/mallActions'
 import Nav from '../Cart/CartTopNav'
 import pay from '../payutil/payUtil'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Snackbar from 'material-ui/Snackbar';
+
+
+
 class Payment extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false,
+        };
+
+    }
+
 
     handleClick(pay_way, price) {
         pay(pay_way, price, function () {
-            alert("付款成功");
+            this.setState({
+                open: true,
+            });
 
         }, function () {
             this.props.history.replace("/payment/succeed")
@@ -22,27 +37,42 @@ class Payment extends React.Component {
         })
     }
 
+    handleRequestClose () {
+        this.setState({
+            open: false,
+        });
+    };
+
     render() {
         console.log(this.props)
         let totalPrice = this.props.homeReducer.cartCircleIcon.totalPrice || this.props.homeReducer.totalPrice;
         let actions = bindActionCreators(mallActions, this.props.dispatch);
         return (
-            <div>
-                <Nav navTitle="结账付款"/>
-                <div style={{position:'relative',top:'120px'}}>
+            <MuiThemeProvider>
+                <div>
+                    <Nav navTitle="结账付款"/>
+                    <div style={{position:'relative',top:'120px'}}>
 
-                    <div className="pay" onClick={()=>{this.handleClick("upacp_wap",totalPrice)}}>
-                        <img src="/imgs/weixin2.png" />付款方式
-                    </div>
+                        <div className="pay" onClick={()=>{this.handleClick("upacp_wap",totalPrice)}}>
+                            <img src="/imgs/weixin2.png" />付款方式
+                        </div>
 
-                    <div className="payAll">
-                        付款金额：<span style={{color:'#EA2735'}}>￥{totalPrice}</span>
+                        <div className="payAll">
+                            付款金额：<span style={{color:'#EA2735'}}>￥{totalPrice}</span>
+                        </div>
                     </div>
+                    <div className="addBottom">
+                        确认付款
+                    </div>
+                    <Snackbar
+                        open={this.state.open}
+                        message="Event added to your calendar"
+                        autoHideDuration={4000}
+                        onRequestClose={this.handleRequestClose.bind(this)}
+                    />
+
                 </div>
-                <div className="addBottom">
-                    确认付款
-                </div>
-            </div>
+            </MuiThemeProvider>
         )
     }
 }
