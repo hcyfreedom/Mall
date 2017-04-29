90103ad1cd20695fd904ce4c13d09635b638c406
hcy <956885844@qq.com>
2017-04-29 14:37:03 +0800

    hcy

---
+++ src/component/Home/Slider/CarouselItem.js
@@ -0,0 +1,24 @@
+/**
+ * Created by hcy on 2017/4/29.
+ */
+import React from 'react'
+import {Carousel} from 'react-bootstrap'
+
+export default class CarouselItem extends React.Component{
+    render(){
+        return(
+            <Carousel >
+                <Carousel.Item style={{fontSize:'60px'}}>
+                    <img width={"100%"} height={'440px'}  src="/imgs/1.jpg"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width={'100%'} height={'440px'}  src="/imgs/2.jpg"/>
+
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width={'100%'} height={'440px'} src="/imgs/1.jpg"/>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
