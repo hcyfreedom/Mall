53ab6e82318c7fac0b024842cd016e22417ed136
hcy <956885844@qq.com>
2017-05-03 16:42:15 +0800

    hcy‘

--- src/pcComponent/Gift/GiftPage.js
+++ src/pcComponent/Gift/GiftPage.js
@@ -1,3 +1,23 @@
 /**
  * Created by hcy on 2017/5/3.
  */
+import React from 'react'
+import NavDetail from '../NavBar/NavDetail'
+import GiftItems from './GiftItem'
+
+export default class GiftPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <NavDetail/>
+                <div style={{display:'flex',justifyContent:'space-around',marginTop:'150px'}}>
+                    <GiftItem/>
+                    <GiftItem/>
+                    <GiftItem/>
+                    <GiftItem/>
+
+                </div>
+            </div>
+        )
+    }
+}
